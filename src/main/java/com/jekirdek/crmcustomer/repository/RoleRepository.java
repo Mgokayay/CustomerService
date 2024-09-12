f5a8d4c4c7e1f1e70262059f52b9aafafdf04f84
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-09-12 21:30:02 +0300

    role repository created

---
+++ src/main/java/com/jekirdek/crmcustomer/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.jekirdek.crmcustomer.repository;
+
+import com.jekirdek.crmcustomer.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
